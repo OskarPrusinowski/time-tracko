8c9593bfef32ce15ab87a018b680cb2d88aa6020
Oskar <prukioksi@gmail.com>
2024-06-06 13:28:17 +0200

    init commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,46 +1,51 @@
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
+import { GitHub } from "@mui/icons-material";
 
 function Login({ loginHandle }) {
     return (
-        <div className="bg-backgroundColor w-full h-screen flex justify-center">
-            <div className="flex border-white mt-48 h-max bg-primaryColor rounded-lg">
-                <form onSubmit={loginHandle} className="flex flex-col p-6">
-                    <TextField
-                        key="email"
-                        id="standard-basic1"
-                        name="email"
-                        label="Email"
-                        variant="outlined"
-                        size="medium"
-                        type="email"
-                        sx={{ width: "400px" }}
-                        required
-                    />
-
-                    <div className="m-4"></div>
-
-                    <TextField
-                        key="password"
-                        id="standard-basic2"
-                        name="password"
-                        label="Password"
-                        variant="outlined"
-                        size="medium"
-                        type="password"
-                        sx={{ width: "400px" }}
-                        required
-                    />
-
-                    <div className="m-4"></div>
-
-                    <Button variant="outlined" type="submit">
-                        Login
-                    </Button>
-                </form>
-            </div>
+   
+        <div class=" block rounded-lg border-indigo-300/100">
+        <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 ">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+         
+          <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-indigo-800">Sign in to your account</h2>
         </div>
-    );
-}
-
-export default Login;
+     
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form onSubmit={loginHandle} class="space-y-6" action="#" method="POST">
+            <div>
+              <label for="email" class="block text-sm font-medium leading-6 text-indigo-900">Email address</label>
+              <div class="mt-2">
+                <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+              </div>
+            </div>
+     
+            <div>
+              <div class="flex items-center justify-between">
+                <label for="password" class="block text-sm font-medium leading-6 text-indigo-900">Password</label>
+                <div class="text-sm">
+                  <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                </div>
+              </div>
+              <div class="mt-2">
+                <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-blue-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+              </div>
+            </div>
+     
+            <div>
+              <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+            </div>
+          </form>
+     
+          <p class="mt-10 text-center text-sm text-gray-500">
+            Not a member?
+            {/* TODO 
+            ADD INTEGRATION WITH GITHUB REST API
+            */}
+            <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"> Login with GitHub  <GitHub/></a>
+          </p>
+        </div>
+      </div>
+      .</div>
+      );
+    }
+export default Login    
